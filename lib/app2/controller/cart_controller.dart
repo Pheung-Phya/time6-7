2bf5dae32d35e6090db2d2901d8c5051d3714ed7
Your Name <you@example.com>
2025-03-11 18:37:51 +0700

    Stop tracking lib/app2

--- lib/app2/controller/cart_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:time6_7/app2/model/shop.dart';
-
-class CartController {
-  static List<Shop> cart = [];
-}
--- lib/app2/controller/like_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:time6_7/app2/model/shop.dart';
-
-class LikeController {
-  static List<Shop> like = [];
-}
